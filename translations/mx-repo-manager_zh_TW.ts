98fa0d99528c4d520ffeff6e7ca9474f23d6f4a5
oldgizahub <geoffgigg@hotmail.com>
2018-02-28 16:05:58 -0500

    Revised am , it, new zh_TW, revised .desktop, .pro for mx-repo-manager

--- translations/mx-repo-manager_zh_TW.ts
+++ translations/mx-repo-manager_zh_TW.ts
@@ -1,10 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_TW" version="2.1">
 <context>
     <name>QApplication</name>
     <message>
         <location filename="../main.cpp" line="53"/>
         <source>You must run this program as root.</source>
-        <translation>Devi eseguire questo programma come amministratore</translation>
+        <translation>本程式必須以 root 身份來執行。</translation>
     </message>
 </context>
 <context>
@@ -16,48 +16,48 @@
         <location filename="../mxrepomanager.cpp" line="401"/>
         <location filename="../mxrepomanager.cpp" line="410"/>
         <source>MX Repo Manager</source>
-        <translation>MX Repo Manager</translation>
+        <translation>MX 軟體倉庫管理員</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="32"/>
         <location filename="../mxrepomanager.cpp" line="472"/>
         <source>Select the APT repository that you want to use:</source>
-        <translation>Scegli il Repository di APT da utilizzare</translation>
+        <translation>選擇你要用的 APT 倉庫：</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="59"/>
         <source>MX repos</source>
-        <translation>MX repos</translation>
+        <translation>MX 倉庫</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="71"/>
         <source>Select fastest MX repo for me</source>
-        <translation>Seleziona l&apos;MX-repo più veloce per me </translation>
+        <translation>替我選擇最快的 MX 倉庫</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="133"/>
         <source>Debian repos</source>
-        <translation>Debian repos</translation>
+        <translation>Debian 倉庫</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="145"/>
         <source>Select fastest Debian repo for me</source>
-        <translation>Seleziona il Debian-repo più veloce per me </translation>
+        <translation>替我選擇最快的 Debian 倉庫</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="193"/>
         <source>Individual sources</source>
-        <translation>Fonti individuali</translation>
+        <translation>個別倉庫</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="254"/>
         <source>Display help </source>
-        <translation>Visualizza la guida</translation>
+        <translation>顯示說明</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="257"/>
         <source>Help</source>
-        <translation>Aiuto</translation>
+        <translation>說明</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="263"/>
@@ -67,12 +67,12 @@
     <message>
         <location filename="../mxrepomanager.ui" line="308"/>
         <source>About this application</source>
-        <translation>Informazioni su questa applicazione</translation>
+        <translation>關於本程式</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="311"/>
         <source>About...</source>
-        <translation>Info...</translation>
+        <translation>關於……</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="317"/>
@@ -82,12 +82,12 @@
     <message>
         <location filename="../mxrepomanager.ui" line="333"/>
         <source>Quit application</source>
-        <translation> Chiudi l&apos;applicazione</translation>
+        <translation>退出程式</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="336"/>
         <source>Close</source>
-        <translation>Chiudi</translation>
+        <translation>關閉</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.ui" line="342"/>
@@ -97,34 +97,34 @@
     <message>
         <location filename="../mxrepomanager.ui" line="358"/>
         <source>Apply</source>
-        <translation>Applica</translation>
+        <translation>使用</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="58"/>
         <source>Please wait...</source>
-        <translation>Aspetta, per piacere...</translation>
+        <translation>請稍待……</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="135"/>
         <location filename="../mxrepomanager.cpp" line="344"/>
         <source>Success</source>
-        <translation>Successo</translation>
+        <translation>成功</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="136"/>
         <location filename="../mxrepomanager.cpp" line="345"/>
         <source>Your new selection will take effect the next time sources are updated.</source>
-        <translation>La tua selezione avrà effetto col prossimo aggiornamento delle sorgenti dei repository.</translation>
+        <translation>下次更新軟體來源時，新選的項目便會啟用。</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="202"/>
         <source>Lists</source>
-        <translation>Elenchi</translation>
+        <translation>列表</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="202"/>
         <source>Sources (checked sources are enabled)</source>
-        <translation>Fonti (le fonti vidimate sono abilitate)</translation>
+        <translation>軟體來源（打勾的項目會啟用）</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="347"/>
@@ -132,59 +132,59 @@
         <location filename="../mxrepomanager.cpp" line="525"/>
         <location filename="../mxrepomanager.cpp" line="540"/>
         <source>Error</source>
-        <translation>Errore</translation>
+        <translation>錯誤</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="348"/>
         <source>Could not change the repo.</source>
-        <translation>Repository non modificabile</translation>
+        <translation>無法改用該軟體倉庫。</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="393"/>
         <source>About MX Repo Manager</source>
-        <translation>Informazioni su MX Repo Manager</translation>
+        <translation>關於 MX 軟體倉庫管理員</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="394"/>
         <source>Version: </source>
-        <translation>Versione:</translation>
+        <translation>版本：</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="395"/>
         <source>Program for choosing the default APT repository</source>
-        <translation>Programma per selezionare il Repository di APT predefinito</translation>
+        <translation>本程式可選擇預設的 APT 倉庫</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="397"/>
         <source>Copyright (c) MX Linux</source>
-        <translation>Copyright (c) MX Linux</translation>
+        <translation>版權所有 (c) MX Linux</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="398"/>
         <location filename="../mxrepomanager.cpp" line="401"/>
         <source>License</source>
-        <translation>Licenza</translation>
+        <translation>授權條款</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="399"/>
         <source>Cancel</source>
-        <translation>Annulla</translation>
+        <translation>取消</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="474"/>
         <source>Select the APT repository and sources that you want to use:</source>
-        <translation>Seleziona il repository di APT e le fonti che vuoi usare:</translation>
+        <translation>選擇你要用的 APT 倉庫和軟體來源：</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="515"/>
         <source>netselect-apt could not detect fastest repo.</source>
-        <translation>netselect-apt non riesce a rilevare il repo più veloce.</translation>
+        <translation>netselect-apt 無法偵測速度最快的倉庫。</translation>
     </message>
     <message>
         <location filename="../mxrepomanager.cpp" line="526"/>
         <location filename="../mxrepomanager.cpp" line="541"/>
         <source>Could not detect fastest repo.</source>
-        <translation>Non si riesce a rilevare il repo più veloce.</translation>
+        <translation>無法偵測到最快的倉庫。</translation>
     </message>
 </context>
 </TS>
