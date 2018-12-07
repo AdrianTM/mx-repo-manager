623849d6a4eb9783aeeefcf578cdfc9ae2ab526f
Adrian <adrian@mxlinux.org>
2018-12-06 22:38:16 -0500

    replace iso.mxrepo with la.mxrepo

--- mxrepomanager.cpp
+++ mxrepomanager.cpp
@@ -362,7 +362,7 @@ void mxrepomanager::replaceRepos(const QString &url)
     // for antiX repos
     QString antix_file = "/etc/apt/sources.list.d/antix.list";
     if (url == "http://mxrepo.com") {
-        repo_line_antix = "http://iso.mxrepo.com/antix/" + ver_name + "/";
+        repo_line_antix = "http://la.mxrepo.com/antix/" + ver_name + "/";
     } else {
         repo_line_antix = url + "/antix/" + ver_name + "/";
     }
