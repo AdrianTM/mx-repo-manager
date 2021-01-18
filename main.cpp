14bdab7815d16ecbed2c0196dd166235f99bea1a
Adrian <adrian@mxlinux.org>
2021-01-17 19:52:24 -0500

    move after translations

--- main.cpp
+++ main.cpp
@@ -39,13 +39,6 @@ int main(int argc, char *argv[])
     app.setWindowIcon(QIcon::fromTheme(app.applicationName()));
     app.setApplicationVersion(VERSION);
 
-    // root guard
-    if (system("logname |grep -q ^root$") == 0) {
-        QMessageBox::critical(nullptr, QObject::tr("Error"),
-                              QObject::tr("You seem to be logged in as root, please log out and log in as normal user to use this program."));
-        exit(EXIT_FAILURE);
-    }
-
     QTranslator qtTran;
     if (qtTran.load(QLocale::system(), "qt", "_", QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
         app.installTranslator(&qtTran);
@@ -57,6 +50,13 @@ int main(int argc, char *argv[])
     QTranslator appTran;
     if (appTran.load(app.applicationName() + "_" + QLocale::system().name(), "/usr/share/" + app.applicationName() + "/locale"))
         app.installTranslator(&appTran);
+
+    // root guard
+    if (system("logname |grep -q ^root$") == 0) {
+        QMessageBox::critical(nullptr, QObject::tr("Error"),
+                              QObject::tr("You seem to be logged in as root, please log out and log in as normal user to use this program."));
+        exit(EXIT_FAILURE);
+    }
 
     if (getuid() == 0) {
         MainWindow w;
