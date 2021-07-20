49fb13a7ad250bba27034e8effa3d768c64ff74e
Adrian <adrian@mxlinux.org>
2021-07-20 16:55:10 -0400

    Save config file under MX-Linux

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon::fromTheme(app.applicationName()));
     app.setApplicationVersion(VERSION);
+    app.setOrganizationName("MX-Linux");
 
     QTranslator qtTran;
     if (qtTran.load(QLocale::system(), "qt", "_", QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
