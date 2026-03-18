a0fe37dc5442c8e224d5d11fcf0c7d1340c16e3c
Adrian <adrian@mxlinux.org>
2026-03-18 13:25:35 -0400

    New build

--- translations/mx-repo-manager_en_US.ts
+++ translations/mx-repo-manager_en_US.ts
@@ -1,62 +1,62 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="es_ES">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="en_US">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../src/mainwindow.ui" line="17"/>
         <location filename="../src/mainwindow.cpp" line="64"/>
         <source>MX Repo Manager</source>
-        <translation>MX Gestor de Repositorios</translation>
+        <translation>MX Repo Manager</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="32"/>
         <location filename="../src/mainwindow.cpp" line="860"/>
         <source>Select the APT repository that you want to use:</source>
-        <translation>Seleccione el repositorio APT que desea usar:</translation>
+        <translation>Select the APT repository that you want to use:</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="59"/>
         <source>MX repos</source>
-        <translation>Repositorios MX</translation>
+        <translation>MX repos</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="84"/>
         <source>Select fastest MX repo for me</source>
-        <translation>Seleccionar el repositorio de MX más rápido para mí</translation>
+        <translation>Select fastest MX repo for me</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="110"/>
         <source>search</source>
-        <translation>Buscar</translation>
+        <translation>search</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="168"/>
         <source>Debian repos</source>
-        <translation>Repositorios Debian</translation>
+        <translation>Debian repos</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="180"/>
         <source>Select fastest Debian repo for me</source>
-        <translation>Seleccionar el repositorio de Debian más rápido para mí</translation>
+        <translation>Select fastest Debian repo for me</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="229"/>
         <source>Individual sources</source>
-        <translation>Fuentes individuales</translation>
+        <translation>Individual sources</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="248"/>
         <source>Restore original APT sources</source>
-        <translation>Restaurar las fuentes originales de APT</translation>
+        <translation>Restore original APT sources</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="329"/>
         <source>Display help </source>
-        <translation>Mostrar la ayuda</translation>
+        <translation>Display help </translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="332"/>
         <source>Help</source>
-        <translation>Ayuda</translation>
+        <translation>Help</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="338"/>
@@ -66,12 +66,12 @@
     <message>
         <location filename="../src/mainwindow.ui" line="383"/>
         <source>About this application</source>
-        <translation>Acerca de esta aplicación</translation>
+        <translation>About this application</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="386"/>
         <source>About...</source>
-        <translation>Acerca de...</translation>
+        <translation>About...</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="392"/>
@@ -81,12 +81,12 @@
     <message>
         <location filename="../src/mainwindow.ui" line="408"/>
         <source>Quit application</source>
-        <translation>Salir de la aplicación</translation>
+        <translation>Quit application</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="411"/>
         <source>Close</source>
-        <translation>Cerrar</translation>
+        <translation>Close</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="417"/>
@@ -96,41 +96,41 @@
     <message>
         <location filename="../src/mainwindow.ui" line="436"/>
         <source>Apply</source>
-        <translation>Aplicar</translation>
+        <translation>Apply</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="191"/>
         <location filename="../src/mainwindow.cpp" line="662"/>
         <location filename="../src/mainwindow.cpp" line="1061"/>
         <source>Success</source>
-        <translation>Exito</translation>
+        <translation>Success</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="192"/>
         <location filename="../src/mainwindow.cpp" line="663"/>
         <location filename="../src/mainwindow.cpp" line="1065"/>
         <source>Your new selection will take effect the next time sources are updated.</source>
-        <translation>Su nueva selección sera efectiva la próxima vez que se actualicen las fuentes.</translation>
+        <translation>Your new selection will take effect the next time sources are updated.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="194"/>
         <source>No Changes</source>
-        <translation>Sin cambios</translation>
+        <translation>No Changes</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="195"/>
         <source>The selected repository is already configured.</source>
-        <translation>El repositorio seleccionado ya está configurado.</translation>
+        <translation>The selected repository is already configured.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="404"/>
         <source>Lists</source>
-        <translation>Listas</translation>
+        <translation>Lists</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="404"/>
         <source>Sources (checked sources are enabled)</source>
-        <translation>Fuentes (las fuentes marcadas están habilitadas)</translation>
+        <translation>Sources (checked sources are enabled)</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="665"/>
@@ -145,37 +145,37 @@
         <location filename="../src/mainwindow.cpp" line="1006"/>
         <location filename="../src/mainwindow.cpp" line="1068"/>
         <source>Error</source>
-        <translation>Error</translation>
+        <translation>Current kernel doesn&apos;t support selected compression algorithm, please edit the configuration file and select a different algorithm.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="665"/>
         <source>Could not change the repo.</source>
-        <translation>So se pudo cambiar el repositorio.</translation>
+        <translation>Could not change the repo.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="690"/>
         <source>Cancel</source>
-        <translation>Cancelar</translation>
+        <translation>Cancel</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="696"/>
         <source>Please wait...</source>
-        <translation>Por favor, espere...</translation>
+        <translation>Please wait...</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="784"/>
         <source>About %1</source>
-        <translation>Acerca de %1</translation>
+        <translation>About %1</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="785"/>
         <source>Version: </source>
-        <translation>Versión: </translation>
+        <translation>Version: </translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="787"/>
         <source>Program for choosing the default APT repository</source>
-        <translation>Programa para elegir el repositorio APT predeterminado</translation>
+        <translation>Program for choosing the default APT repository</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="789"/>
@@ -185,42 +185,42 @@
     <message>
         <location filename="../src/mainwindow.cpp" line="790"/>
         <source>%1 License</source>
-        <translation>%1 Licencia</translation>
+        <translation>%1 License</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="804"/>
         <source>%1 Help</source>
-        <translation>%1 Ayuda</translation>
+        <translation>%1 Help</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="821"/>
         <source>Warning</source>
-        <translation>Advertencia</translation>
+        <translation>Warning</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="822"/>
         <source>You have selected MX Test Repo. It&apos;s not recommended to leave it enabled or to upgrade all the packages from it.</source>
-        <translation>Ha seleccionado MX Repositorios de Prueba. No se recomienda dejarlo habilitado o actualizar todos los paquetes.</translation>
+        <translation>You have selected MX Test Repo. It&apos;s not recommended to leave it enabled or to upgrade all the packages from it.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="825"/>
         <source>A safer option is to install packages individually with MX Package Installer.</source>
-        <translation>Una opción más segura es instalar paquetes individualmente con MX Instalador de Paquetes.</translation>
+        <translation>A safer option is to install packages individually with MX Package Installer.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="862"/>
         <source>Select the APT repository and sources that you want to use:</source>
-        <translation>Seleccione el repositorio APT y las fuentes que desea usar:</translation>
+        <translation>Select the APT repository and sources that you want to use:</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="928"/>
         <source>netselect-apt could not detect fastest repo.</source>
-        <translation>netselect-apt no pudo detectar el repositorio más rápido.</translation>
+        <translation>netselect-apt could not detect fastest repo.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="1068"/>
         <source>Could not restore the original APT source files.</source>
-        <translation>No se pudo restaurar los archivos de fuentes APT originales</translation>
+        <translation>Could not restore the original APT source files.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="901"/>
@@ -228,42 +228,42 @@
         <location filename="../src/mainwindow.cpp" line="949"/>
         <location filename="../src/mainwindow.cpp" line="964"/>
         <source>Could not detect fastest repo.</source>
-        <translation>No se pudo detectar el repositorio más rápido.</translation>
+        <translation>Could not detect fastest repo.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="977"/>
         <source>Can&apos;t figure out if this app is running on antiX or MX</source>
-        <translation>No puedo determinar si esta aplicación se ejecuta en antiX o MX</translation>
+        <translation>Can&apos;t figure out if this app is running on antiX or MX</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="985"/>
         <source>MX version not detected or out of range: </source>
-        <translation>Versión de MX no detectada o fuera de rango:</translation>
+        <translation>MX version not detected or out of range: </translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="998"/>
         <source>Could not download original APT files.</source>
-        <translation>No se pudieron descargar los archivos APT originales.</translation>
+        <translation>Could not download original APT files.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="1006"/>
         <source>Could not unzip downloaded file.</source>
-        <translation>No se pudo descomprimir el archivo descargado.</translation>
+        <translation>Could not unzip downloaded file.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="1049"/>
         <source>Enabling AHS</source>
-        <translation>Habilitar AHS</translation>
+        <translation>Enabling AHS</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="1049"/>
         <source>Do you use AHS (Advanced Hardware Stack) repo?</source>
-        <translation>¿Utilizará el repo de AHS (Advanced Hardware Stack)?</translation>
+        <translation>Do you use AHS (Advanced Hardware Stack) repo?</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="1062"/>
         <source>Original APT sources have been restored to the release status. User added source files in /etc/apt/sources.list.d/ have not been touched.</source>
-        <translation>Las fuentes APT originales se han restaurado al estado de liberacion. Los archivos de fuentes añadidos por el usuario en /etc/apt/sources.list.d/ no se han tocado.</translation>
+        <translation>Original APT sources have been restored to the release status. User added source files in /etc/apt/sources.list.d/ have not been touched.</translation>
     </message>
 </context>
 <context>
@@ -271,39 +271,39 @@
     <message>
         <location filename="../src/about.cpp" line="49"/>
         <source>License</source>
-        <translation>Licencia</translation>
+        <translation>License</translation>
     </message>
     <message>
         <location filename="../src/about.cpp" line="50"/>
         <location filename="../src/about.cpp" line="60"/>
         <source>Changelog</source>
-        <translation>Registro de cambios</translation>
+        <translation>Changelog</translation>
     </message>
     <message>
         <location filename="../src/about.cpp" line="51"/>
         <source>Cancel</source>
-        <translation>Cancelar</translation>
+        <translation>Cancel</translation>
     </message>
     <message>
         <location filename="../src/about.cpp" line="73"/>
         <source>&amp;Close</source>
-        <translation>&amp;Cerrar</translation>
+        <translation>&amp;Close</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="83"/>
         <location filename="../src/main.cpp" line="92"/>
         <source>Error</source>
-        <translation>Error</translation>
+        <translation>Current kernel doesn&apos;t support selected compression algorithm, please edit the configuration file and select a different algorithm.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="84"/>
         <source>You seem to be logged in as root, please log out and log in as normal user to use this program.</source>
-        <translation>Parece que ha iniciado sesión como root, cierre la sesión e inicie sesión como usuario normal para usar este programa.</translation>
+        <translation>You seem to be logged in as root, please log out and log in as normal user to use this program.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="93"/>
         <source>You must run this program with admin access.</source>
-        <translation>Debe ejecutar este programa con acceso de administrador.</translation>
+        <translation>You must run this program with admin access.</translation>
     </message>
 </context>
 </TS>
